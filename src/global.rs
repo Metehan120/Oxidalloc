0eb212479d8ed7be6b8a4e82e5c28fa1d955dbb2
Metehan120 <metehan@zaferoglu.me>
2025-11-06 02:30:38 +0300

    Memory usage Optimization

--- src/global.rs
+++ src/global.rs
@@ -49,7 +49,7 @@ impl GlobalHandler {
 
     #[allow(unsafe_op_in_unsafe_fn)]
     pub unsafe fn pop_batch_from_global(&self, class: usize, batch_size: usize) -> *mut Header {
-        for _ in 0..2 {
+        loop {
             let current_head = GLOBAL_MAP_LIST[class].load(Ordering::Acquire);
 
             if current_head.is_null() {
@@ -74,7 +74,5 @@ impl GlobalHandler {
                 return current_head;
             }
         }
-
-        null_mut()
     }
 }
