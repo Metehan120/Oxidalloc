27042d5faf1a3804a94e486480f5c187ac5a01b3
Metehan120 <metehan@zaferoglu.me>
2026-01-16 20:39:34 +0300

    small fix on bootstrap

--- src/va/bootstrap.rs
+++ src/va/bootstrap.rs
@@ -10,7 +10,6 @@ use std::{
 use crate::{
     OX_ENABLE_EXPERIMENTAL_HEALING, OX_MAX_RESERVATION, OX_TRIM_THRESHOLD, OX_USE_THP,
     slab::thread_local::{THREAD_REGISTER, ThreadLocalEngine},
-    va::align_to,
 };
 
 pub static IS_BOOTSTRAP: AtomicBool = AtomicBool::new(true);
@@ -117,9 +116,7 @@ pub unsafe fn init_reverse() {
             val = 1024 * 1024 * 256;
         }
 
-        OX_MAX_RESERVATION.store(align_to(val, 4096), Ordering::Relaxed);
-    } else {
-        OX_MAX_RESERVATION.store(1024 * 1024 * 1024 * 64, Ordering::Relaxed);
+        OX_MAX_RESERVATION.store(val.next_power_of_two(), Ordering::Relaxed);
     }
 }
 
