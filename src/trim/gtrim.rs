9ffed3d0d64f578c63668cf4a4fe26ac4b5aed18
Metehan120 <metehan@zaferoglu.me>
2026-01-25 05:32:33 +0300

    gtrim fix

--- src/trim/gtrim.rs
+++ src/trim/gtrim.rs
@@ -1,8 +1,8 @@
 use std::{ffi::c_void, ptr::null_mut, sync::atomic::Ordering};
 
 use crate::{
-    AVERAGE_BLOCK_TIMES_GLOBAL, FREED_MAGIC, HEADER_SIZE, MAX_NUMA_NODES, OX_CURRENT_STAMP,
-    OxHeader, OxidallocError,
+    AVERAGE_BLOCK_TIMES_GLOBAL, HEADER_SIZE, MAGIC, MAX_NUMA_NODES, OX_CURRENT_STAMP, OxHeader,
+    OxidallocError,
     slab::{
         NUM_SIZE_CLASSES, SIZE_CLASSES, get_size_4096_class,
         global::{GLOBAL, GlobalHandler},
@@ -29,7 +29,7 @@ impl GTrim {
         let mut real = 1;
 
         while real < 16 && !(*block).next.is_null() && is_ours((*block).next as usize) {
-            if (*block).magic == FREED_MAGIC {
+            if (*block).magic == MAGIC {
                 OxidallocError::MemoryCorruption.log_and_abort(
                     block as *mut c_void,
                     "Find in_use block during PThread Trim / Memory Corruption",
