2dacdb7967ffc018d1373213a064d32a7d73b85f
Metehan120 <metehan@zaferoglu.me>
2026-02-06 22:55:12 +0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -51,13 +51,12 @@ pub static mut FREED_MAGIC: u8 = 1;
 pub static OX_GLOBAL_STAMP: OnceLock<Instant> = OnceLock::new();
 pub static mut OX_CURRENT_STAMP: u32 = 0;
 
-pub static AVERAGE_BLOCK_TIMES_PTHREAD: AtomicUsize = AtomicUsize::new(3000);
-pub static AVERAGE_BLOCK_TIMES_GLOBAL: AtomicUsize = AtomicUsize::new(3000);
-
+pub static AVERAGE_BLOCK_TIMES_GLOBAL: AtomicUsize = AtomicUsize::new(3);
 pub static OX_TRIM_THRESHOLD: AtomicUsize = AtomicUsize::new(1024 * 1024 * 10);
+pub static OX_MAX_RESERVATION: AtomicUsize = AtomicUsize::new(1024 * 1024 * 1024 * 16);
+
 pub static mut OX_FORCE_THP: bool = false;
 pub static mut OX_TRIM: bool = true;
-pub static OX_MAX_RESERVATION: AtomicUsize = AtomicUsize::new(1024 * 1024 * 1024 * 16);
 
 pub fn get_clock() -> &'static Instant {
     OX_GLOBAL_STAMP.get_or_init(|| Instant::now())
